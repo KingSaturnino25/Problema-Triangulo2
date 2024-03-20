ce732f4e5e611d9efc178dc948b3cba24ec449c1
Caio Saturnino <caiosaturnino27@gmail.com>
2024-03-20 09:46:16 -0300

    Atualizando program cs

--- Problema Triângulo2/Program.cs
+++ Problema Triângulo2/Program.cs
@@ -6,7 +6,16 @@ namespace Problema_Triângulo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double xA, xB, xC, yA, yB, yC;
+            xA = 3.00;
+            xB = 4.00;
+            xC = 5.00;
+            yA = 7.50;
+            yB = 4.50;
+            yC = 4.02;
+
+            double p = (xA + xB + xC) / 2;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
         }
     }
 }
